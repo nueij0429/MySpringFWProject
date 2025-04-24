6bb80510ba9193404a6ff54bffafe54f8a3440eb
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-24 15:53:13 +0900

    StringPrinter, ConsolePrinter 생성자 생성

--- src/main/java/myspring/di/xml/Hello.java
+++ src/main/java/myspring/di/xml/Hello.java
@@ -8,10 +8,11 @@ public class Hello {
 	List<String> names;
 
 	public Hello() {
-		System.out.println("");
+		System.out.println(this.getClass().getName() + " 기본생성자 호출됨!");
 	}
 
 	public Hello(String name, Printer printer) {
+		System.out.println(this.getClass().getName() + " 오버로딩 생성자 호출됨!");
 		this.name = name;
 		this.printer = printer;
 	}
