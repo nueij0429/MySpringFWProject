806be782c80a0ef9bd2476ea660884511ce6cfb8
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-25 18:00:18 +0900

    Autowired 어노테이션 연결

---
+++ src/test/java/mylab/order/di/xml/OrderSpringTest.java
@@ -0,0 +1,18 @@
+package mylab.order.di.xml;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:mylab-order-di.xml")
+public class OrderSpringTest {
+	@Autowired
+	OrderService service;
+	
+	@Autowired
+	ShoppingCart cart;
+	
+	
+}
