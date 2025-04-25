b1e3fd4af24a00003b788223719abc6efb837231
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-25 19:17:19 +0900

    1-2 실습 문제 완료

--- src/test/java/mylab/user/di/annot/UserServiceTest.java
+++ src/test/java/mylab/user/di/annot/UserServiceTest.java
@@ -1,21 +1,41 @@
 package mylab.user.di.annot;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = "classpath:mylab.user.di.xml")
+@ContextConfiguration(locations = "classpath:mylab-user-di.xml")
 public class UserServiceTest {
 	
 	@Autowired
 	UserService userService;
 	
+	@Value("${userId}")
+	private String testUserId;
+
+	@Value("${userName}")
+	private String testUserName;
+
+	@Value("${userPassword}")
+	private String testUserPassword;
+
+	
 	@Test
 	void testUserService() {
+		assertNotNull(userService);
+		assertNotNull(userService.getUserRepository());
 		
+		assertEquals("MySQL", userService.getUserRepository().getDbType());
+		
+		assertNotNull(userService.getSecurityService());
+		
+		assertTrue(userService.registerUser("123", "홍길동", "12345"));
 	}
 
 }
