50160701b044bec4598b4f2056445da922f3cf99
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-25 16:49:16 +0900

    values.properties 생성

--- src/test/java/myspring/di/annot/HelloBeanSpringTest.java
+++ src/test/java/myspring/di/annot/HelloBeanSpringTest.java
@@ -2,6 +2,8 @@ package myspring.di.annot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import javax.annotation.Resource;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,12 +13,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:hello-di.xml")
 public class HelloBeanSpringTest {
-
 	@Autowired
 	HelloBean hello;
+	
+	@Resource(name = "stringPrinter")
+	PrinterBean printer;
 	
 	@Test
 	void helloBean() {
 		assertEquals("Hello 어노테이션", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 어노테이션", printer.toString());
 	}
 }
