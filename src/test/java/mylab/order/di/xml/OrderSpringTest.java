f0dace666ebeec3509b41f6628e3f650e42afa1a
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-25 18:09:56 +0900

    xml에 OrderService 추가 생성

--- src/test/java/mylab/order/di/xml/OrderSpringTest.java
+++ src/test/java/mylab/order/di/xml/OrderSpringTest.java
@@ -1,5 +1,9 @@
 package mylab.order.di.xml;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -8,11 +12,33 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:mylab-order-di.xml")
 public class OrderSpringTest {
-	@Autowired
-	OrderService service;
 	
 	@Autowired
 	ShoppingCart cart;
 	
+	@Autowired
+	OrderService service;
 	
+	@Test
+	void testShoppingCart() {
+		assertNotNull(cart);
+		assertEquals(2, cart.getProducts().size());
+		
+		assertEquals(30000, cart.getTotalPrice());
+		
+		for(Product product : cart.getProducts()) {
+			System.out.println(product);
+		}
+	}
+	
+	@Test
+	void testOrderService() {
+		assertNotNull(service);
+		assertEquals(30000, service.calculateOrderTotal());
+		
+		
+		for(Product product : service.getShoppingCart().getProducts()) {
+			System.out.println(product);
+		}
+	}
 }
