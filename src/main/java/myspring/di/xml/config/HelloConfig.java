b85c9b786a38a00c2dd740943cb452b182dc7071
nueij0429 <ssjjjieun0429@gmail.com>
2025-04-28 16:04:58 +0900

    maria db 연동 시작

---
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -0,0 +1,50 @@
+package myspring.di.xml.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
+import myspring.di.xml.StringPrinter;
+
+@Configuration
+@PropertySource(value = "classpath:values.properties")
+public class HelloConfig {
+	
+	@Autowired
+	private Environment env;
+
+	@Bean
+	public Printer stringPrinter() {
+		return new StringPrinter();
+	}
+	
+	@Bean
+	public Printer consolePrinter() {
+		return new ConsolePrinter();
+	}
+	
+	@Bean
+	public Hello hello() {
+		Hello hello = new Hello(); //alt+shift+l을 누르면 변수가 자동으로 생긴다.
+		//hello.setName("스프링")과 동일. env를 사용하는 이유: 나중에 값이 변경될 수 있기 때문.
+		hello.setName(env.getProperty("myName1")); //"스프링"
+		hello.setPrinter(stringPrinter());
+		hello.setNames(namesList());
+		return hello;
+	}
+	
+	@Bean
+	public List<String> namesList() {
+		String names = env.getProperty("names.list.of.strings");
+		String[] strArray = names.split(",");
+		return Arrays.asList(strArray);
+	}
+}
---
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -0,0 +1,46 @@
+package myspring.di.xml.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = HelloConfig.class)
+public class HelloConfigTest {
+
+	@Autowired
+	List<String> namesList;
+	
+	@Autowired
+	Hello hello;
+	
+	@Resource(name = "stringPrinter")
+	Printer printer;
+	
+	@Test
+	void testHelloConfig() {
+		assertEquals("Hello 스프링", hello.sayHello());
+		hello.print();
+		assertEquals("Hello 스프링", printer.toString());
+		assertEquals("Java", hello.getNames().get(0));
+	}
+	
+	@Test
+	void testNameList() {
+		//foreach +ctrl+space
+		for(String name : namesList) {
+			System.out.println(name);
+		}
+	}
+}
